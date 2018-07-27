74d8bf49d06a8fb205b173ea4fad417ab58de08d
AJ <ashleyannjorgensen@gmail.com>
2018-07-26 18:53:43 -0700

    homework commit

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,15 +1,10 @@
 /* set up the required libraries/tools */
 var mysql = require("mysql");
-const readline = require('readline');
-
-/* setup the input reader */
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline');
+var inquirer = require('inquirer');
 
 
-/* set up connection to the mysql server */
+// set up connection to the mysql server 
 var connection = mysql.createConnection({
   host: "localhost",
 
@@ -24,39 +19,161 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-/* function that fetches & displays all items */
+// use connection to run functions/program 
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  queryAllItems();
+});
+
+var currentInventory = [];
+var itemIdArray = [];
+var itemNameArray = [];
+var itemPriceArray = [];
+var purchaseQuantity;
+var productId;
+
+
+// function that fetches & displays all items 
 function queryAllItems() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    for (var i = 0; i < res.length; i++) {
-      console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity );
+  connection.query("SELECT * FROM products", function (err, res) {
+    currentInventory = res;
+
+
+    for (var i = 0; i < currentInventory.length; i++) {
+      itemIdArray.push(currentInventory[i].item_id);
+      itemNameArray.push(currentInventory[i].product_name);
+      itemPriceArray.push(currentInventory[i].price);
     }
+    displayInventory();
+
+
     console.log("-----------------------------------");
+    inquirer.prompt([
+      {
+        name: "buyProduct",
+        type: "input",
+        message: "What's the ID of the product you want to buy?",
+        validate: function (value) {
+          if (isNaN(value) == false) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+      {
+        name: "howManyUnits",
+        type: "input",
+        message: "How many units do you want to buy?",
+        validate: function (value) {
+          if (isNaN(value) == false) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+    ]).then(answers => {
+      productId = parseInt(answers.buyProduct);
+      if (itemIdArray.indexOf(productId) > -1){
+        purchaseQuantity = answers.howManyUnits;
+        
+        if(checkInventory(productId, purchaseQuantity) == true)
+        {
+          console.log("Your order is being processed!");
+          updateInventory(productId, purchaseQuantity);
+          process.exit();
+        }
+        else{
+          console.log("Not enough quantity");
+        }
+
+
+      } else {
+        console.log("Please enter a valid ID number.");
+        setTimeout(queryAllItems, 2000);
+      }
+    });
   });
 }
 
-/* use connection to run functions/program */
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  
-  queryAllItems();
-  rl.pause();
-  
-  /* ask the user what they want to buy */
-  rl.question('What do you want to buy? ', (item_id) => {
-    // TODO: Log the answer in a database
-    console.log(`Thank you for your valuable feedback: ${item_id}`);
-     /* ask the user how many units */
-    rl.question('How many do you want to buy? ', (item_qty) => {
-      // TODO: Log the answer in a database
-      console.log(`Thank you for your valuable feedback: ${item_qty}`);
-      rl.close();
-    });
-  });
+// checkInventory should take in parameters ID and QTY and return true/false depending if there's enough qty
+function checkInventory() {
+  //console.log(currentInventory);
+  // 
+  //console.log(currentInventory[itemIdArray.indexOf(productId)].stock_quantity);
+  var selectedItemInventory = currentInventory[itemIdArray.indexOf(productId)].stock_quantity;
 
-  console.log("wocka wocka wocka");
+  if (purchaseQuantity <= selectedItemInventory) {
+    //call and update inventory function that we need to create 
+    // updateInventory(productId, purchaseQuantity)
+    return true;
+    
+    //subtract purcahse quantity 
+    //then you will pass that information into the new amount and pass that into the update quantity. 
+  }
+  else {
+    //ifw e cannot fulfill the order console log please select another item
+    console.log("Unable to process; quantity insufficient. Please try again.");
+    //then call queryAllItems function again
+    queryAllItems();
+    return false;
+  }
+}
 
-});
+//updateInventory should take an ID and QTY and update the database (but not display any price/total/etc)
+//great bay activity update item in a database update query!
+function updateInventory(updateWhatId, updateWhatQty) {
+  var index = itemIdArray.indexOf(updateWhatId);
+  currentInventory[index].stock_quantity -= updateWhatQty;
+  var query = "UPDATE products SET stock_quantity=stock_quantity-" + updateWhatQty + " WHERE item_id=" + updateWhatId + ";";
+  //console.log(query);
+  connection.query(query);  
+
+
+  displayInventory();
+  var updatedName = itemNameArray[index];
+  var updatedPrice = itemPriceArray[index];
+  console.log("You bought " + updateWhatQty + " of ID#" + updateWhatId + ": " + updatedName + " at $" + updatedPrice + " each");
+  console.log("Total: $" + updatedPrice*updateWhatQty);
+}
+
+// display all inventory items
+function displayInventory() {
+  for (var i = 0; i < currentInventory.length; i++) {
+    console.log(currentInventory[i].item_id + " | " + currentInventory[i].product_name + " | " + currentInventory[i].department_name + " | " + currentInventory[i].price + " | " + currentInventory[i].stock_quantity);
+  }
+}
+
+//displayPurchaseTotal takes in purchaseId and purchaseQty and displays the total price (and nothing else)
+
+
+
+
+
+
+  // queryAllItems();
+  // rl.pause();
+
+  // /* ask the user what they want to buy */
+  // rl.question('What do you want to buy? ', (item_id) => {
+  //   // TODO: Log the answer in a database
+  //   console.log(`Thank you for your valuable feedback: ${item_id}`);
+  //    /* ask the user how many units */
+  //   rl.question('How many do you want to buy? ', (item_qty) => {
+  //     // TODO: Log the answer in a database
+  //     console.log(`Thank you for your valuable feedback: ${item_qty}`);
+  //     rl.close();
+  //   });
+  // });
+
+  // console.log("wocka wocka wocka");
+
+
+  // queryALlItems();
+  
+
 
 
 
